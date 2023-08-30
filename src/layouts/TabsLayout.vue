e2a087267f04197d3337f6fd7f2563daca34e44c
Jeremy Dulon <jdulon@ccmbenchmark.com>
2023-08-30 08:42:43 +0200

    fix(ui): show correctly active route in tabs

--- src/layouts/TabsLayout.vue
+++ src/layouts/TabsLayout.vue
@@ -4,11 +4,11 @@
       <router-view :key="$route.fullPath" />
     </q-page-container>
     <q-footer class="text-primary">
-      <q-tabs active-color="amber-5" indicator-color="transparent" class="text-grey">
-        <q-route-tab icon="fas fa-fire-alt" :to="{ name: 'home' }" />
-        <q-route-tab icon="fas fa-search" :to="{ name: 'search' }" />
-        <q-route-tab icon="fas fa-star" :to="{ name: 'favorites' }" />
-        <q-route-tab icon="fas fa-cog" :to="{ name: 'userConfig' }" />
+      <q-tabs v-model="$route.name" active-color="amber-5" indicator-color="transparent" class="text-grey">
+        <q-route-tab name="home" icon="fas fa-fire-alt" :to="{ name: 'home' }" />
+        <q-route-tab name="search" icon="fas fa-search" :to="{ name: 'search' }" />
+        <q-route-tab name="favorites" icon="fas fa-star" :to="{ name: 'favorites' }" />
+        <q-route-tab name="userConfig" icon="fas fa-cog" :to="{ name: 'userConfig' }" />
         <q-tab icon="fas fa-sign-out-alt" v-on:click="logout" />
       </q-tabs>
     </q-footer>
